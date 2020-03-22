79150ed1e5a8cc275a7df70bb4693f3b6c99a081
Ashok Yadav Yadav <Ashok_Yadav2@Dell.com>
2020-03-22 15:17:00 +0530

    section # commit

--- DatingApp.API/Data/DataContext.cs
+++ DatingApp.API/Data/DataContext.cs
@@ -1,4 +1,5 @@
 using DatingApp.API.Controllers.Models;
+using DatingApp.API.Models;
 using Microsoft.EntityFrameworkCore;
 
 namespace DatingApp.API
@@ -7,5 +8,6 @@ namespace DatingApp.API
     {
         public DataContext(DbContextOptions<DataContext> options):base(options){}
         public DbSet<Value> Values { get; set; }    
+        public DbSet<User> Users {get;set;}
     }
 }
